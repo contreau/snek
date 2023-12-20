feb4456b95f5d3efc5dc3a8b574c7042aac4d1e6
zenDev-2 <cfk102@gmail.com>
2023-12-20 00:48:51 -0500

    style additions

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,16 @@
   // Supports weights 400-700
   import "@fontsource-variable/pixelify-sans";
   import GameGrid from "./lib/GameGrid.svelte";
+  import { currentScore, highScore } from "./lib/store";
 </script>
 
 <main>
   <h1>snek</h1>
-  <p>definitely not snake</p>
+  <div class="hud-grid">
+    <p><span style="color: #ff46b5;">high score:</span> {$highScore}</p>
+    <p>definitely not snake</p>
+    <p><span style="color: #ff46b5;">current score:</span> {$currentScore}</p>
+  </div>
   <GameGrid></GameGrid>
 </main>
 
@@ -16,11 +21,17 @@
   h1 {
     font-family: "Pixelify Sans Variable", sans-serif;
     margin-top: 0;
-    margin-bottom: 0rem;
+    margin-bottom: 1rem;
     font-weight: 200;
     font-size: clamp(2.5rem, 10vw, 3.8rem);
   }
   p {
     margin-top: 0;
+  }
+
+  .hud-grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 5.5rem;
   }
 </style>
